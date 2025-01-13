3e8f9a6b994fdbee73388f16ebee04edaeb64dbd
root <root@LAPTOP-A1TR7OSU.>
2025-01-13 16:36:47 +0900

    new file

--- lv/4/10807.cc
+++ lv/4/10807.cc
@@ -4,6 +4,18 @@ using namespace std;
 int main(void){
     ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-    
+    int n,m,r=0;
+    int arr[100];
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    cin>>m;
+    for(int i=0;i<n;i++){
+        if(arr[i]==m){
+            r+=1;
+        }
+    }
+    cout<<r;
     return 0;
 }
