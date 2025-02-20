e7b93c179d12fae545102ec049f031aad8f34937
Boo <boosunung@gmail.com>
2025-02-20 20:20:47 +0900

    깨달음

--- lv/23/12865.cc
+++ lv/23/12865.cc
@@ -4,23 +4,18 @@ using namespace std;
 
 int dp[100001]; // 1차원 배열 사용
 int n, k;
+int w,v;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    cin >> n >> k;
-    
-    for (int i = 0; i < n; i++) {
-        int w, v;
-        cin >> w >> v;
-        
-        // 💡 뒤에서부터 갱신 (무게가 작은 것부터 갱신하면 덮어씌워짐)
-        for (int j = k; j >= w; j--) {
-            dp[j] = max(dp[j], dp[j - w] + v);
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+        cin>>w>>v;
+        for(int j=k;j>=w;j--){
+            dp[j]=max(dp[j],dp[j-w]+v);
         }
     }
-
-    cout << dp[k] << '\n'; // 최댓값 출력
+    cout<<dp[k]<<endl;
     return 0;
 }
